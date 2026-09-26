e05512f1e906e09af2a9e87d53041c1969b5dd60
nick1234-design <Nicholasborgards1@gmail.com>
2026-09-25 17:30:07 -0700

    Update client.js

--- providers/febbox/client.js
+++ providers/febbox/client.js
@@ -270,16 +270,33 @@ async function getVideoQualityLinks({
     );
   }
 
-  const data = await febboxGet(
-  '/console/video_quality_list',
+  const targetPostData = new URLSearchParams();
+targetPostData.append('fid', String(fid));
+targetPostData.append('share_key', shareKey);
+
+const axiosResponse = await axios.post(
+  `${FEBBOX_ORIGIN}/file/player`,
+  targetPostData.toString(),
   {
-    token,
-    shareKey,
-    params: {
-      fid,
-      share_key: shareKey
-    }
+    headers: {
+      'User-Agent': USER_AGENT,
+      'X-Requested-With': 'XMLHttpRequest',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json, text/javascript, */*; q=0.01',
+      'Referer': `${FEBBOX_ORIGIN}/share/${shareKey}`,
+      'Cookie': toCookieHeader(token)
+    },
+    timeout: DEFAULT_TIMEOUT_MS,
+    validateStatus: () => true,
+    maxRedirects: 3
   }
+);
+
+const data = axiosResponse.data;
+
+console.log(
+  '[FebBox Direct Debug] file/player response:',
+  JSON.stringify(data)
 );
 console.log(
   '[FebBox Direct Debug] video_quality_list response:',
